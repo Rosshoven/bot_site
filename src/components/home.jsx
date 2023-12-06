a5bba9a7cbd7c0bc9b355c0482f7e9aabece5511
Rosshoven <Michaeledwardross@gmail.com>
2023-12-06 13:42:43 -0600

    clean up

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -13,29 +13,29 @@ export default function Home() {
     }
 
     return (
-        <div>
-            {/* BANNER */}
+    
+    <div>
+        
+        {/* BANNER */}
             <img style=
             // using rem and position fixed keeps the banner in place
             {{marginTop: '4.4rem', 
             width: '100%',
             position: 'static',
             }} 
-            src={Banner} className="img-fluid" alt="Boys On Trial Banner">
-            </img>
+            src={Banner} className="img-fluid" alt="Boys On Trial Banner" />
+           
 
-            {/* ALBUMS */}
+        {/* ALBUMS */}
             <div style={{
                 display: 'flex',
                 justifyContent: 'space-around',
                 flexWrap: 'wrap',
                 flexDirection: "row", 
                 backgroundColor: 'pink',
-                // alignItems: 'center',
                 border: '1px solid silver',
-                // padding: '20px',
-                // marginTop: '2rem'
                 }}>
+
                 <Album image={IITLTWD} 
                         title={`If it's the last thing we do...`}
                         description={'album cover for IITLTWD'}/>
@@ -43,12 +43,20 @@ export default function Home() {
                         title={`¡Guantanamo Boy!`} 
                         description={'album cover for ¡Guantanamo Boy!'}
                         />
-
+                <Album image={IITLTWD} 
+                        title={`If it's the last thing we do...`}
+                        description={'album cover for IITLTWD'}/>
+                <Album image={Guan}
+                        title={`¡Guantanamo Boy!`} 
+                        description={'album cover for ¡Guantanamo Boy!'}
+                        />        
             </div>
+            
+        {/* Footer */}
             <div style={{display: 'flex', justifyContent: 'left', background: 'black'}}>
                 <h6 style={footerStyle}>&nbsp;©2023DADADesigns&nbsp;</h6> 
                 <h6 style={footerStyle}>&nbsp;&nbsp;Photos by Johanna&nbsp;</h6> 
-                <h6 style={footerStyle}>&nbsp;&nbsp;Logo by Blake&nbsp;</h6>
+                <h6 style={footerStyle}>&nbsp;&nbsp;Logo by Blake&nbsp;</h6>                
             </div>
 
 
