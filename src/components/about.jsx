16c1b559fa4b44b9ec4f02833c5deb9a4696817c
Rosshoven <Michaeledwardross@gmail.com>
2024-01-09 19:16:10 -0600

    Filling in quotes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,20 +8,25 @@ export default function About() {
 return (
 <div className='quoteWrapper'>
     <div id="lineOne">
-      <QuoteBlock text="The only limit to our realization of tomorrow will be our doubts of today." author="Franklin D. Roosevelt" />
+      <QuoteBlock text="Boys On Trial have everything that makes music matter: passion, compassion, deft playing, great sound, all delivered with attitude." author="Barb Wire, WLUW Chicago" />
     </div>
 
     <div id="lineTwo">
-      <QuoteBlock text="The only limit to our realization of tomorrow will be our doubts of today." author="Franklin D. Roosevelt" />
+      <QuoteBlock text="They often carry a punk rock spirit without falling into a single mold. This trio deserve some credit for taking chances" author="Illinois Entertainer" />
     </div>
 
     <div id="lineThree">
-      <QuoteBlock text="The only limit to our realization of tomorrow will be our doubts of today." author="Franklin D. Roosevelt" />
+      <QuoteBlock text="Boys On Trial are assembling an army of intricate words, blazing bass lines and heart punch drums which are filtering up through the hallways of Chicago's underground." author="Craig Turnwall, Fulcrum Press" />
     </div>
 
     <div id="lineFour">
-      <QuoteBlock text="The only limit to our realization of tomorrow will be our doubts of today." author="Franklin D. Roosevelt" />
+      <QuoteBlock text="Listening to the Fall and '90s Dischord releases will prepare you for the onslaught of these boys. High production values along with distinct vocals make a unique listening experience." author="Punk Planet" />
     </div>
+
+    <div id="lineFive">
+      <QuoteBlock text="Imagine all the people listening to Boys On Trial" author="John Lennon, The Quarrymen" />
+    </div>
+
 </div>    
   );
 };
